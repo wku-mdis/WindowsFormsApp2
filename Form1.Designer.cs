aeeac3317746fbde4a8df24ea1745db7cc29d7b2
William Ku <kucheech@gmail.com>
2020-12-28 21:27:20 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(635, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(39, 167);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1000, 210);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.dataGridView1_UserDeletingRow);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(39, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(561, 38);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "SELECT * FROM soft562.facebook_users";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Get all facebook users",
+            "Get all customers",
+            "Get all guys in facebook"});
+            this.comboBox1.Location = new System.Drawing.Point(50, 92);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(445, 24);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(635, 105);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(59, 21);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Male";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(715, 105);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(75, 21);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Female";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(266, 407);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(95, 37);
+            this.buttonAdd.TabIndex = 6;
+            this.buttonAdd.Text = "Insert";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(484, 407);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(95, 37);
+            this.buttonEdit.TabIndex = 7;
+            this.buttonEdit.Text = "Update";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(695, 407);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(95, 37);
+            this.buttonDelete.TabIndex = 8;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1099, 580);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonDelete;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        int selectedID = 0;
+        int selectedRow;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            displayForm();
+        }
+
+      
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string[] queries = { 
+                "SELECT * FROM soft562.facebook_users",
+                "SELECT * FROM soft562.customers",
+                "SELECT * FROM soft562.facebook_users where gender = 'male'",
+            };
+
+            string query = queries[comboBox1.SelectedIndex];
+            //MessageBox.Show(query);
+
+            performSQLCommand(query);
+
+        }
+        
+        private void displayForm()
+        {
+            performSQLCommand("SELECT * FROM soft562.facebook_users");
+            buttonAdd.Enabled = false;
+            buttonEdit.Enabled = false;
+            buttonDelete.Enabled = false;
+            dataGridView1.Columns[0].ReadOnly = true;
+        }
+
+        private void performSQLCommand(string query)
+        {
+            /////////////////////////////////////////////////////////////////
+            //
+            // The basic format of a connection string consists of a series
+            // of keyword/value pairs separated by semicolons. The equal
+            // sign (=) connects each keyword and its value. Specifically,
+            // our connection string MUST refer to xServe as the server, and
+            // to our username and password to access xServe.
+            //
+            /////////////////////////////////////////////////////////////////
+            string connectionString = "SERVER=" + DBConnect.SERVER + ";" +
+                "DATABASE=" + DBConnect.DATABASE_NAME + ";" + "UID=" +
+                DBConnect.USER_NAME + ";" + "PASSWORD=" +
+                DBConnect.PASSWORD + ";" + "SslMode=" +
+                DBConnect.SslMode + ";";
+            ///////////////////////////////////////////////////////////////
+            //
+            // Create an open connection to a MySQL Server database. In our
+            // case, the connection allows us to interact with xServe.
+            //
+            ///////////////////////////////////////////////////////////////
+            using (MySqlConnection connection =
+                new MySqlConnection(connectionString))
+            {
+                ///////////////////////////////////////////////////////////
+                //
+                // The query which retrieves all the records in the
+                // customers table.
+                //
+                ///////////////////////////////////////////////////////////
+
+                ///////////////////////////////////////////////////////////
+                //
+                // Opens a database connection with the property settings
+                // specified by the ConnectionString.
+                //
+                ///////////////////////////////////////////////////////////
+                connection.Open();
+
+                ///////////////////////////////////////////////////////////
+                //
+                // Submit the SQL statement to be executed against the
+                // MySQL database.
+                //
+                ///////////////////////////////////////////////////////////
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                ///////////////////////////////////////////////////////////
+                //
+                // The MySqlDataAdapter represents a set of data commands
+                // and a database connection that are used to fill a
+                // dataset and update a MySQL database.
+                //
+                ///////////////////////////////////////////////////////////
+                MySqlDataAdapter sqlDA = new MySqlDataAdapter(cmd);
+                DataTable customerDataTable = new DataTable();
+                try
+                {
+                    sqlDA.Fill(customerDataTable);
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message);
+                }
+
+                ///////////////////////////////////////////////////////////
+                //
+                // Bind the ACME customer table to the Data Grid View.
+                //
+                ///////////////////////////////////////////////////////////
+                dataGridView1.DataSource = customerDataTable;
+
+            } // End of using (MySqlConnection connection = ...
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+          private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            selectedRow = e.RowIndex;
+            DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+            try
+            {
+                selectedID = Convert.ToInt32(row.Cells[0].Value.ToString());
+
+            } catch(Exception ex)
+            {
+                selectedID = 0;
+            }
+
+            if (selectedID > 0)
+            {
+                buttonAdd.Enabled = false;
+                buttonEdit.Enabled = true;
+                buttonDelete.Enabled = true;
+            } else
+            {
+                buttonAdd.Enabled = true;
+                buttonEdit.Enabled = false;
+                buttonDelete.Enabled = false;
+            }
+            //MessageBox.Show($"{selectedID}");
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {/*
+            int ID = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            string firstname = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            string lastname = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            string query = $"update soft562.facebook_users set FirstName = '{firstname}' where UserID = {ID}";
+            performSQLCommand(query);
+            performSQLCommand("SELECT * FROM soft562.facebook_users");
+            */
+        }
+
+      
+
+        private void dataGridView1_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            /*
+            int ID = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            string firstname = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            string lastname = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            string query = $"INSERT INTO soft562.facebook_users (FirstName, LastName) VALUES({firstname},{lastname}";
+            performSQLCommand(query);
+            performSQLCommand("SELECT * FROM soft562.facebook_users");
+            */
+        }
+
+        private void dataGridView1_UserAddedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            MessageBox.Show("UserAddedRow");
+            /*
+            DataGridViewRow r = e.Row;
+            
+            string firstname = r.Cells[1].Value.ToString();
+            string lastname =r.Cells[2].Value.ToString();
+            string query = $"INSERT INTO soft562.facebook_users (FirstName, LastName) VALUES({firstname},{lastname}";
+            performSQLCommand(query);
+            performSQLCommand("SELECT * FROM soft562.facebook_users");
+            */
+            
+        }
+
+        private void dataGridView1_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        {
+            MessageBox.Show("dataGridView1_UserDeletingRow");
+
+            DataGridViewRow r = e.Row;
+            string ID = r.Cells[0].Value.ToString();
+            string query = $"DELETE FROM soft562.facebook_users WHERE UserID = {ID}";
+            performSQLCommand(query);
+            performSQLCommand("SELECT * FROM soft562.facebook_users");
+        }
+
+        private void dataGridView1_NewRowNeeded(object sender, DataGridViewRowEventArgs e)
+        {
+            MessageBox.Show("NewRowNeeded");
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            string firstName = dataGridView1.Rows[selectedRow].Cells[1].Value.ToString();
+            string lastName = dataGridView1.Rows[selectedRow].Cells[2].Value.ToString();
+
+            string message = $"Are you sure you want to add this record? First name: { firstName }";
+            string title = "Add record";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                string query = $"INSERT INTO soft562.facebook_users (FirstName, LastName) VALUES('{firstName}', '{lastName}')";
+                performSQLCommand(query);
+                displayForm();
+            }
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            int userID = Convert.ToInt32(dataGridView1.Rows[selectedRow].Cells[0].Value.ToString());
+            string firstName = dataGridView1.Rows[selectedRow].Cells[1].Value.ToString();
+            string lastName = dataGridView1.Rows[selectedRow].Cells[2].Value.ToString();
+
+            string message = $"Are you sure you want to update this record? First name: { firstName }";
+            string title = $"Update record for userID {userID}";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                string query = $"UPDATE soft562.facebook_users SET FirstName = '{firstName}', LastName = '{lastName}' WHERE UserID = {userID}";
+                performSQLCommand(query);
+                displayForm();
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            string message = "Are you sure you want to delete this record?";
+            string title = "Delete record";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                string query = $"DELETE from soft562.facebook_users WHERE UserID = {selectedID}";
+                performSQLCommand(query);
+                displayForm();
+            }
+            
+        }
+    }
+}
